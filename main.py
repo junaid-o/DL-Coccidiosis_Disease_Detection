1d098b46493a0e8527255858784b36416d0ca999
junaid-o <junaidmohd795@gmail.com>
2023-08-22 03:59:07 +0530

    base model preparation done

--- main.py
+++ main.py
@@ -1,3 +1,60 @@
 from CoccidiosisDetector.logger import logging
+from CoccidiosisDetector.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CoccidiosisDetector.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CoccidiosisDetector.pipeline.stage_03_training import ModelTrainingPipeline
+from CoccidiosisDetector.pipeline.stage_04_evaluation import EvaluationPipeline
 
-logging.info(f"{'>>' * 20} Model trainer log started {'<<' * 20} ")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logging.info(f"*******************")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logging.info(f"*******************")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logging.info(f"*******************")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logging.exception(e)
+        raise e
+
