980624ac4ab693df5b26ee154c6e8824f64f05a4
junaid-o <junaidmohd795@gmail.com>
2023-08-22 15:58:55 +0530

    Flask app created

--- src/CoccidiosisDetector/pipeline/stage_04_evaluation.py
+++ src/CoccidiosisDetector/pipeline/stage_04_evaluation.py
@@ -22,12 +22,12 @@ class EvaluationPipeline:
 
 if __name__ == '__main__':
     try:
-        logger.info(f"*******************")
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        logging.info(f"*******************")
+        logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = EvaluationPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
-        logger.exception(e)
+        logging.exception(e)
         raise e
             
